566f992cb298dfe8e4fc63f84beed4852598c15c
Camila Matos Spolador <cmspola@gmail.com>
2023-07-26 09:49:43 -0300

    Initial Commit

---
+++ BochechudoNano1/BochechudoNano1App.swift
@@ -0,0 +1,17 @@
+//
+//  BochechudoNano1App.swift
+//  BochechudoNano1
+//
+//  Created by Camila Spolador on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BochechudoNano1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
